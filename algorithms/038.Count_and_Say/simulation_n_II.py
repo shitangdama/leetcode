90a8b4b008b884d212ff4a7540d34ebb2cfbe44e
shitangdama <1012351692@qq.com>
2015-08-06 22:22:00 +0800

    update

---
+++ algorithms/038.Count_and_Say/simulation_n_II.py
@@ -0,0 +1,26 @@
+# Time:  O(n * 2^n)
+# Space: O(2^n)
+
+class Solution:
+    # @return a string
+    def countAndSay(self, n):
+        seq = "1"
+        for i in xrange(n - 1):
+            seq = self.getNext(seq)
+        return seq
+    
+    def getNext(self, seq):
+        i, next_seq = 0, ""
+        while i < len(seq):
+            cnt = 1        
+            while i < len(seq) - 1 and seq[i] == seq[i + 1]:
+                cnt += 1
+                i += 1
+            next_seq += "{}{}".format(cnt, seq[i])
+            i += 1
+        return next_seq
+
+if __name__ == "__main__":
+    for i in xrange(1, 4):
+        print Solution().countAndSay(i)
+
