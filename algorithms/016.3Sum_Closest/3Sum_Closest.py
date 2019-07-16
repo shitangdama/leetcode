0f2980cd096e582510489fb1911ea22ffcb1b4e2
shitangdama <kbr1990117@gmail.com>
2019-07-17 00:33:41 +0800

    add_16

--- algorithms/016.3Sum_Closest/3Sum_Closest.py
+++ algorithms/016.3Sum_Closest/3Sum_Closest.py
@@ -1,33 +1,27 @@
-# 类似于上一题
 class Solution:
-    # @return an integer
     def threeSumClosest(self, nums, target):
-        nums = sorted(nums)
-        # 先定义几个变量
-        # 最小差距
-        min_diff = float("inf")
-        # 结果
-        result = float("inf")
-        # 三个指针
-        i, j, k = 0, 1, len(nums)-1
-        while i < len(nums)-2:
-            # 从新赋值
-            j = i+1
-            k = len(nums)-1
-            while j<k:
-                # 判断有几种情况
+        nums.sort()
+        i = 0 
+        
+        min_diff = 99
+        result = 99
+        
+        while i < len(nums) - 2:
+            j, k = i+1, len(nums)-1
+            
+            while j < k:
                 diff = nums[i] + nums[j] + nums[k] - target
                 if abs(diff) < min_diff:
                     min_diff = abs(diff)
                     result = nums[i] + nums[j] + nums[k]
                 if diff < 0:
-                    j = j+1
+                    j = j + 1
                 elif diff > 0:
-                    k = k-1
+                    k = k - 1
                 else:
                     return result
             
-            i += 1
+            i = i + 1
             while  i < len(nums) - 2 and nums[i] == nums[i-1]:
                 i = i+1
         return result
